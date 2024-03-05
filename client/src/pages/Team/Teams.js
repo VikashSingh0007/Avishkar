eb54fb499421fd6adbc760f6b83f779e0da5a0f3
Vikash Singh <97822077+VikashSingh0007@users.noreply.github.com>
2024-03-05 20:02:52 +0530

    Json data added in teams page

---
+++ client/src/pages/Team/Teams.js
@@ -0,0 +1,116 @@
+export const team = [
+    {
+      department: "Director",
+      data: [
+        {
+          image:
+            "https://img.freepik.com/free-photo/christian-religion-elements-about-faith-god_23-2150641341.jpg?w=996&t=st=1708364202~exp=1708364802~hmac=5f072ba86f81b72351ebe0f052c7de369434b115555f327619c3d5008ca736c1",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "singhvikash7077",
+        },
+        // Add more data for Festival Secretary...
+      ],
+    },
+    {
+      department: "Sac President",
+      data: [
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        // Add more data for SAC President...
+      ],
+    },
+    {
+      department: "Festival Secretary",
+      data: [
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        // Add more data for Festival Secretary...
+      ],
+    },
+    {
+      department: "Web Team",
+      data: [
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        // Add more data for Web Team...
+      ],
+    },
+    {
+      department: "PR Team",
+      data: [
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        {
+          image:
+            "https://img.freepik.com/free-photo/lone-tree_181624-46361.jpg?w=826&t=st=1708281792~exp=1708282392~hmac=26efaff03bbeb31aae8c88f8804b19a17299e5b2f1e98d9b60c224a8824a9ebf",
+          name: "Abcc",
+          Phone: "1234567890",
+          instagramId: "@abcc_insta",
+        },
+        // Add more data for Web Team...
+      ],
+    },
+  ];
