20e161ad8c5535037016829fa5b3249f243c5ed2
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-05 17:09:02 +0530

    modified landing page

--- client/src/style.js
+++ client/src/style.js
@@ -1,14 +1,14 @@
 const styles = {
-  boxWidth: "xl:max-w-[1680px] w-full",
+  boxWidth: " xl:max-w-[1680px] w-full",
 
   heading2:
-    "font-poppins font-bold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
+    "font-poppins  font-bold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
   heading3:
     "font-poppins font-semibold xs:text-[40px] text-[32px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
   paragraph: "font-poppins font-medium text-white text-[18px] leading-[30.8px]",
 
   flexCenter: "flex justify-center items-center",
-  flexStart: "flex justify-center items-start",
+  flexStart: "flex  mb-28 lg:mb-0 justify-center items-start",
 
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
@@ -19,8 +19,8 @@ const styles = {
 };
 
 export const layout = {
-  section: `flex md:flex-row flex-col ${styles.paddingY}`,
-  sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+  section: `flex lg:flex-row flex-col ${styles.paddingY}`,
+  sectionReverse: `flex lg:flex-row flex-col-reverse ${styles.paddingY}`,
 
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
   sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
