08b9927d9d663428417155cec32e170c26747fb1
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2024-03-07 02:48:35 +0530

    removed FAQ

--- client/src/pages/Home/EventTimeline.jsx
+++ client/src/pages/Home/EventTimeline.jsx
@@ -5,84 +5,84 @@ const timelineData = [
     text: "Inaugural Ceremony, gnoTalks  ",
     date: "March 16 2024",
     category: {
-      tag: "Day-1",
+      tag: "Day1",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day1",
+      text: "Get Schedule of Day 1",
     },
   },
   {
     text: "gnoTalks",
     date: "March 17 2024",
     category: {
-      tag: "Day-2",
+      tag: "Day2",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day2",
+      text: "Get Schedule of Day 2",
     },
   },
   {
     text: "Special Performance",
     date: "March 18 2024",
     category: {
-      tag: "Day-3",
+      tag: "Day3",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day3",
+      text: "Get Schedule of Day 3",
     },
   },
   {
     text: "KavyasAndhya",
     date: "March 19 2024",
     category: {
-      tag: "Day-4",
+      tag: "Day4",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day4",
+      text: "Get Schedule of Day 4",
     },
   },
   {
     text: "EDM Night",
     date: "March 20 2024",
     category: {
-      tag: "Day-5",
+      tag: "Day5",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day5",
+      text: "Get Schedule of Day 5",
     },
   },
   {
     text: "BAnd Performance",
     date: "March 21 2024",
     category: {
-      tag: "Day-6",
+      tag: "Day 6",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day6",
+      text: "Get Schedule of Day 6",
     },
   },
   {
     text: "Celebrity Night",
     date: "April 22 2024",
     category: {
-      tag: "Day-7",
+      tag: "Day7",
       color: "#000",
     },
     link: {
       url: "",
-      text: "Get Schedule of Day7",
+      text: "Get Schedule of Day 7",
     },
   },
 ];
@@ -91,10 +91,19 @@ const TimelineItem = ({ data }) => (
   <div className="timeline-item protest-revolution-regular rumoura-font ">
     <div className="timeline-item-content protest-revolution-regular rumoura-font">
       <span
-        className="tag protest-revolution-regular rumoura-font"
-        style={{ background: data.category.color }}
+        className="tag rumoura-font"
+        style={{
+          background: data.category.color,
+          display: "inline-flex",
+          flexDirection: "column",
+          fontSize: "15px",
+        }}
       >
-        {data.category.tag}
+        {data.category.tag.split("").map((char, index) => (
+          <span key={index} style={{}}>
+            {char}
+          </span>
+        ))}
       </span>
       <time style={{ color: "black", fontSize: "20px" }}>{data.date}</time>
 
@@ -119,40 +128,6 @@ const TimelineItem = ({ data }) => (
 const Timeline = () =>
   timelineData.length > 0 && (
     <>
-      {/* <div>
-        <div className="bike">
-          <ul className="wheel l-wheel">
-            {[...Array(12)].map((_, index) => (
-              <li key={index} className={`stick${index + 1}`}></li>
-            ))}
-          </ul>
-          <ul className="wheel r-wheel">
-            {[...Array(12)].map((_, index) => (
-              <li key={index} className={`stick${index + 1}`}></li>
-            ))}
-          </ul>
-          <div className="frame frame1"></div>
-          <div className="frame frame2"></div>
-          <div className="frame frame3"></div>
-          <div className="frame frame4"></div>
-          <div className="frame frame5"></div>
-          <div className="frame frame6"></div>
-          <div className="cushion"></div>
-          <div className="head">
-            <div className="handler"></div>
-          </div>
-          <div className="axle">
-            <div className="stick stick1"></div>
-            <div className="stick stick2"></div>
-            <div className="pedal pedal1"></div>
-            <div className="pedal pedal2"></div>
-          </div>
-          <div className="wheel-axle">
-            <div className="chain chain1"></div>
-            <div className="chain chain2"></div>
-          </div>
-        </div>
-      </div> */}
       <h2
         className="text-3xl text-black font-bold mb-4 flex justify-center mt-[10%] rumoura-font"
         style={{
