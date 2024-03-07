50924b4b3e2ed71fccadf5d6df20aba261ff08d3
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2024-03-07 15:12:50 +0530

    user data

--- client/src/pages/user/View.jsx
+++ client/src/pages/user/View.jsx
@@ -9,7 +9,7 @@ const View = () => {
     username: "USERNAME",
     email: "EMAIL@NCNNC.COM",
     resumeLink: "HTTP://XNSBCNBCNX.COM",
-    mobileNo: "735764765",
+    phone: "735764765",
     gender: "MALE",
     collegeName: "MNNIT",
   });
@@ -31,14 +31,19 @@ const View = () => {
 
     fetchData();
   }, []);
-
+  console.log(fetchedData);
   return (
     <>
       <div className="flex items-center justify-center h-auto w-[90%] ">
         <div className="  bg-gradient-to-b from-gray-600 via-gray-700 to-gray-900 rounded-lg p-8 shadow-md text-center text-white">
-          <h2 className="text-[3em] font-bold text-orange-500 mb-3">John Doe</h2>
-          <div className="text-gray-200 mb-2 text-xl">Email: john.doe@example.com</div>
-          <div className="text-gray-200 text-xl">Phone: +1 123-456-7890</div>
+          <h2 className="text-[3em] font-bold text-orange-500 mb-3">
+            {fetchedData.name}
+          </h2>
+          <div className="text-gray-200 mb-2 text-xl">
+            UserId - {fetchedData.username}
+          </div>
+          <div className="text-gray-200 mb-2 text-xl">{fetchedData.email}</div>
+          <div className="text-gray-200 text-xl">{fetchedData.phone}</div>
         </div>
       </div>
     </>
