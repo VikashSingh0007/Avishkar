0f71d5683685f9d5e7563e3de4ec23b6fed92f69
Mrinal <varshneymrinal2005@gmail.com>
2024-03-08 03:18:36 +0530

    Enhancement: Profile page

--- client/src/pages/user/Resume.jsx
+++ client/src/pages/user/Resume.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 
 const ResumeUploadContainer = () => {
   const [resumeLink, setResumeLink] = useState("");
@@ -9,10 +9,20 @@ const ResumeUploadContainer = () => {
   };
 
   return (
-    <div className="w-[90%] md:w-[50%] mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Upload Resume</h2>
+    <div
+      className="w-[90%] md:w-[50%] mx-auto p-6  bg-gradient-to-b from-[#63462D] rounded-lg shadow-md"
+      style={{
+        boxShadow: "0px 10px 10px black",
+      }}
+    >
+      <h2 className="lg:text-[3em] md:text-[2em] text-[1.5em] text-white font-bold mb-4">
+        Upload Resume
+      </h2>
       <div className="mb-4">
-        <label htmlFor="resumeLink" className="text-lg block font-semibold mb-2">
+        <label
+          htmlFor="resumeLink"
+          className="lg:text-[1.5em] md:text-[1em] text-white block font-semibold mb-2"
+        >
           Resume Drive Link
         </label>
         <input
@@ -24,10 +34,18 @@ const ResumeUploadContainer = () => {
           className="w-full p-2 border rounded-md"
         />
       </div>
-      <div className="text-gray-600 text-sm mb-4">Make sure it is public</div>
+      <div
+        className="text-gray-200 text-sm mb-4
+      lg:text-[1.2em] md:text-[1em] text-[0.5em]"
+      >
+        Make sure it is public
+      </div>
       <button
-        className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600"
+        className="lg:text-[1.8em] md:text-[1.2em] text-[0.8em] bg-[#63462D] text-white px-4 py-2 rounded-md hover:bg-[#423C31]"
         onClick={handleUploadResume}
+        style={{
+        boxShadow: "0px 5px 5px black",
+      }}
       >
         Upload Resume
       </button>
