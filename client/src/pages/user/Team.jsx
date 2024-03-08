b24670d65dc1a83e97322ec8b0dad5bf83928aff
lokesh-wagh <129964049+lokesh-wagh@users.noreply.github.com>
2024-03-08 17:03:37 +0530

    mailing service migration

--- client/src/pages/user/Team.jsx
+++ client/src/pages/user/Team.jsx
@@ -80,8 +80,9 @@ const Team = () => {
                 style={{ boxShadow: "0 2px 5px 0 black" }}
               >
                 <div className="text-white lg:text-[1.3em] md:text-[1em] text-[0.8em] font-medium mb-2">
-                  {member.username}
+                  {member.name}
                 </div>
+                
                 <div className="text-[#423C31] font-bold lg:text-[1em] md:text-[0.8em] text-[0.5em]">{member.role}</div>
               </div>
             ))}
