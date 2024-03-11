78f274040053a90a0b5d67cd1a61e0a24118d6bc
Shivansh mishra <125989942+Shivansh-243@users.noreply.github.com>
2024-03-12 00:37:26 +0530

    kredomania updated

--- client/src/Data/Event/Avishkar/Kreedomania.js
+++ client/src/Data/Event/Avishkar/Kreedomania.js
@@ -8,6 +8,7 @@ import img6 from "../../../../src/Data/Images/Credomania/6.png";
 import img7 from "../../../../src/Data/Images/Credomania/7.png";
 import img8 from "../../../../src/Data/Images/Credomania/8.png";
 import img9 from "../../../../src/Data/Images/Credomania/9.png";
+import img4 from "../../../../src/Data/Images/Credomania/4.png";
 import img10 from "../../../../src/Data/Images/Credomania/10.png";
 import img11 from "../../../../src/Data/Images/Credomania/11.png";
 import img12 from "../../../../src/Data/Images/Credomania/12.png";
@@ -24,11 +25,24 @@ export const Kreedomania = [
 
         rule: [
           {
-            rule_1: "PLAYED IN A 5V5 TEAM FORMAT",
-            rule_2: "LEAGUE MATCHES WILL OCCUR IN ROUND ROBIN MODE",
-            rule_3: "MAP FOR LEAGUE MATCHES TO BE DECIDED BY ORGANISERS",
-            rule_4: "FINALS TO BE PLAYED ON 3 MAPS",
-            rule_5: "TEAMS HAVE TO BRING THEIR OWN SETUP",
+            rule_1:
+              "Follow a knockout format with prelims held online on 12th March.",
+            rule_2:
+              "The final 2 teams will play 3 matches, and the team with the highest cumulative score will win.",
+            rule_3: "Participants must play on their own setup.",
+            rule_4:
+              "Matchmaking will be done randomly, and brackets will be released 12 hours before prelims.",
+            rule_5:
+              "Teams must be ready to play within 15 minutes of the commencement time of their match; any further delay might result in disqualification, and the opponent team will be promoted to the next round.",
+            rule_6:
+              "Players must have a Valorant account eligible for competitive games to compete. This account must be used for all matches during the competition.",
+            rule_7:
+              "The Riot accounts used in the competition must be made public for thorough scrutiny.",
+            rule_8:
+              "Participants are allowed to compete from only one team during the competition.",
+            rule_9:
+              "Any sort of misconduct during the event will lead to disqualification.",
+            rule_10: "The organizers' decision will be final.",
           },
         ],
 
@@ -49,37 +63,45 @@ export const Kreedomania = [
           },
         ],
       },
-      {
-        name: "CSGO 2.0",
-        disc: "",
-        BGImageLink: img8,
-        rule: [
-          {
-            rule_1: "PLAYED IN A 5V5 TEAM FORMAT.",
-            rule_2: "LEAGUE MATCHES WILL OCCUR IN ROUND ROBIN MODE",
-            rule_3: "MAP FOR LEAGUE MATCHES TO BE DECIDED BY ORGANISERS",
-            rule_4: "FINALS TO BE PLAYED ON 3 MAPS",
-            rule_5: "TEAMS HAVE TO BRING THEIR OWN SETUP",
-          },
-        ],
+      // {
+      //   name: "CSGO 2.0",
+      //   disc: "",
+      //   BGImageLink: img8,
+      //   rule: [
+      //     {
+      //       // these are of fifa
+      //       rule_1:
+      //         "Participants are allowed to choose any regular season team available in the original game, excluding all-star teams. The first player to make a choice will be determined by a toss.",
+      //       rule_2:
+      //         "Overtime: If the scores are level after the regular time duration of play, the game will proceed directly to a penalty shootout. Game settings: Default.",
+      //       rule_3:
+      //         "The full tournament will be held offline from 10th March 2023, and it will be a Knockout tournament. In case of a tie, 2 matches will be played in 1v1 format, and the number of goals will decide the winner.",
+      //       rule_4:
+      //         "Two pauses of 10 seconds each will be allowed to each player during the entire game, excluding the half-time stoppage. Game settings: Default.",
+      //       rule_5:
+      //         "Participants are expected to adhere to these rules at all times, and any violations will be subject to penalties as determined by the judges. The decision made by the judges will be final with no further discussion.",
+      //       rule_6: "Organizers' decision will be final.",
+      //       rule_7: "Game settings: Default.",
+      //     },
+      //   ],
 
-        // BGImageLink: "",
-        PsLink: "",
-        coordinators: [
-          {
-            name: "Divyansh Gangwar",
-            phone: "9758987445",
-          },
-          {
-            name: "Nikhil Chaurasia",
-            phone: "9335183583",
-          },
-          {
-            name: "Shaurya Raj Srivastav",
-            phone: "8299760453",
-          },
-        ],
-      },
+      //   // BGImageLink: "",
+      //   PsLink: "",
+      //   coordinators: [
+      //     {
+      //       name: "Divyansh Gangwar",
+      //       phone: "9758987445",
+      //     },
+      //     {
+      //       name: "Nikhil Chaurasia",
+      //       phone: "9335183583",
+      //     },
+      //     {
+      //       name: "Shaurya Raj Srivastav",
+      //       phone: "8299760453",
+      //     },
+      //   ],
+      // },
 
       {
         name: "FIFA’23",
@@ -87,8 +109,18 @@ export const Kreedomania = [
         BGImageLink: img12,
         rule: [
           {
-            rule_1: "MATCHES PLAYED IN STANDARD 1V1 MODE",
-            rule_2: "CONTROLLER AND PC SETUP TO BE PROVIDED BY COORDINATORS",
+            rule_1:
+              "Participants are allowed to choose any regular season team available in the original game, excluding all-star teams. The first player to make a choice will be determined by a toss.",
+            rule_2:
+              "Overtime: If the scores are level after the regular time duration of play, the game will proceed directly to a penalty shootout. Game settings: Default.",
+            rule_3:
+              "The full tournament will be held offline from 10th March 2023, and it will be a Knockout tournament. In case of a tie, 2 matches will be played in 1v1 format, and the number of goals will decide the winner.",
+            rule_4:
+              "Two pauses of 10 seconds each will be allowed to each player during the entire game, excluding the half-time stoppage. Game settings: Default.",
+            rule_5:
+              "Participants are expected to adhere to these rules at all times, and any violations will be subject to penalties as determined by the judges. The decision made by the judges will be final with no further discussion.",
+            rule_6: "Organizers' decision will be final.",
+            rule_7: "Game settings: Default.",
           },
         ],
 
@@ -114,38 +146,38 @@ export const Kreedomania = [
         ],
       },
 
-      {
-        name: "MORTAL KOMBAT",
-        disc: "",
-        BGImageLink: img16,
-        rule: [
-          {
-            rule_1: "Rules- MATCHES PLAYED IN 1V1 BATTLE FORMAT",
-            rule_2: "CONTROLLER & PC SETUP TO BE PROVIDED BY COORDINATORS",
-          },
-        ],
+      // {
+      //   name: "MORTAL KOMBAT",
+      //   disc: "",
+      //   BGImageLink: img16,
+      //   rule: [
+      //     {
+      //       rule_1: "Rules- MATCHES PLAYED IN 1V1 BATTLE FORMAT",
+      //       rule_2: "CONTROLLER & PC SETUP TO BE PROVIDED BY COORDINATORS",
+      //     },
+      //   ],
 
-        //  BGImageLink: "",
-        PsLink: "",
-        coordinators: [
-          {
-            name: "Divyansh Gangwar",
-            phone: "9758987445",
-          },
-          {
-            name: "Nikhil Chaurasia",
-            phone: "9335183583",
-          },
-          {
-            name: "Shaurya Raj Srivastav",
-            phone: "8299760453",
-          },
-          {
-            name: "Utkarsh Chauhan",
-            phone: "9119391227",
-          },
-        ],
-      },
+      //   //  BGImageLink: "",
+      //   PsLink: "",
+      //   coordinators: [
+      //     {
+      //       name: "Divyansh Gangwar",
+      //       phone: "9758987445",
+      //     },
+      //     {
+      //       name: "Nikhil Chaurasia",
+      //       phone: "9335183583",
+      //     },
+      //     {
+      //       name: "Shaurya Raj Srivastav",
+      //       phone: "8299760453",
+      //     },
+      //     {
+      //       name: "Utkarsh Chauhan",
+      //       phone: "9119391227",
+      //     },
+      //   ],
+      // },
 
       {
         name: "MINI MILITIA",
@@ -153,11 +185,16 @@ export const Kreedomania = [
         BGImageLink: img2,
         rule: [
           {
-            rule_1: "PLAYED IN A 4V4 TEAM FORMAT",
-            rule_2: "LEAGUE MATCHES WILL OCCUR IN ROUND ROBIN MODE",
-            rule_3: "MAP FOR LEAGUE MATCHES TO BE DECIDED BY ORGANISERS",
-            rule_4: "FINALS TO BE PLAYED ON 3 MAPS",
-            rule_5: "TEAMS HAVE TO BRING THEIR OWN SETUP",
+            rule_1:
+              "The game will be played in a team death match format with a team size of 2.",
+            rule_2:
+              "Tournament will be played on the apk provided by the organisers",
+            rule_3: "Map will be decided by the organisers on a random basis.",
+            rule_4:
+              "All teams will be divided in groups and 3 team death match will be played. The top 1 or 2 teams will proceed to the next round.",
+            rule_5:
+              "Finals will be played on 3 maps and the collective ranking system will be used for the final result.",
+            rule_6: "Matches will be played offline",
           },
         ],
 
@@ -189,11 +226,16 @@ export const Kreedomania = [
         BGImageLink: img9,
         rule: [
           {
-            rule_1: "PLAYED IN A 4v4 TEAM FORMAT.",
-            rule_2: "LEAGUE MATCHES WILL OCCUR IN ROUND ROBIN MODE",
-            rule_3: "MAP FOR LEAGUE MATCHES TO BE DECIDED BY ORGANISERS",
-            rule_4: "FINALS TO BE PLAYED ON 3 MAPS",
-            rule_5: "TEAMS HAVE TO BRING THEIR OWN SETUP",
+            rule_1: "ALL THE TEAMS WILL BE DIVIDED INTO SUBGROUPS",
+            rule_2:
+              "3 BATTLE ROYALE WILL BE PLAYED AND YOU’LL BE AWARDED POINTS IN EACH BR ACCORDING TO THE FOLLOWING POINT SYSTEM - #1 - 15 POINTS #2 - 12 POINTS #3 - 10 POINTS #4 - 8 POINTS #5 - 6 POINTS  #6 - 4 POINTS #7 - 2 POINTS  #(8-12) - 1 POINTS (AND 1 POINT FOR EACH KILL FOR ALL TEAMS)",
+            rule_3:
+              "TOP 8 TEAMS WILL MOVE ON TO THE NEXT ROUND FROM EACH GROUP.",
+            rule_4:
+              "3 BATTLE ROYALE WILL BE PLAYED IN THE FINALS AND SAME POINT SYSTEM WILL BE FOLLOWED.",
+            rule_5: "ORGANIZERS DECISIONS WILL BE FINAL",
+            rule_6:
+              "ROOM ID AND PASSWORD WILL BE SHARED 15 MINUTES PRIOR TO THE ALLOTED TIMINGS AND MATCHES WILL START ON TIME, WE’LL NOT WAIT FOR ANYONE.",
           },
         ],
 
@@ -224,11 +266,17 @@ export const Kreedomania = [
         BGImageLink: img5,
         rule: [
           {
-            rule_1: "PLAYED IN A 4v4 TEAM FORMAT.",
-            rule_2: "LEAGUE MATCHES WILL OCCUR IN ROUND ROBIN MODE",
-            rule_3: "MAP FOR LEAGUE MATCHES TO BE DECIDED BY ORGANISERS",
-            rule_4: "FINALS TO BE PLAYED ON 3 MAPS",
-            rule_5: "TEAMS HAVE TO BRING THEIR OWN SETUP",
+            rule_1: "All the teams will be divided into sub-groups",
+            rule_2:
+              "3 battle royale will be played and you’ll be awarded points in each BR according to the following point system - #1 - 15 points #2 - 12 points #3 - 10 points #4 - 8 points #5 - 6 points  #6 - 4 points  #7 - 2 points  #(8-12) - 1 points (and 1 point for each kill for all teams)",
+            rule_3:
+              "Top 8 teams will move on to the next round from each group.",
+            rule_4:
+              "The next final round will be a series of battle royale consisting of 3 rounds",
+            rule_5: "same point system will be followed.",
+            rule_6: "organizers decisions will be final",
+            rule_7:
+              "Room ID and password will be shared 15 minutes prior to the alloted timings and matches will start on time, we’ll not wait for anyone.",
           },
         ],
 
@@ -259,9 +307,13 @@ export const Kreedomania = [
         BGImageLink: img7,
         rule: [
           {
-            rule_1: "Rules: Each team should have at least two players",
-            rule_2: "Points are typically awarded for each correct guess",
-            rule_3: "The team with the most points at the end of the game wins",
+            rule_1:
+              "THE PLATFORM THAT WOULD BE USED FOR THE EVENT IS SKRIBBL.IO, SO PARTICIPANTS ARE STRONGLY RECOMMENDED TO GET ACQUAINTED WITH IT.",
+            rule_2: "PARTICIPANTS MUST HAVE A STABLE NETWORK CONNECTION.",
+            rule_3:
+              "AS THE GAME COMMENCES, PARTICIPANTS ARE NOT ALLOWED TO WRITE ANY WORD OR ANY LETTER ON THE SCREEN",
+            rule_4:
+              "NO EXTERNAL HELP FROM THE INTERNET/ANOTHER PERSON WILL BE TOLERATED DURING THE EVENT.",
           },
         ],
 
@@ -286,41 +338,41 @@ export const Kreedomania = [
           },
         ],
       },
-      {
-        name: "ROCKET LEAGUE",
-        disc: "",
-        BGImageLink: img15,
-        rule: [
-          {
-            rule_1: "MAX TEAM SIZE: 3",
-            rule_2: "The arena for each match WILL be predetermined",
-            rule_3: "TEAMS HAVE TO BRING THEIR OWN SETUP",
-            rule_4:
-              "In the event of a tied game , overtime periods are typically played until a golden goal (sudden-death goal) is scored",
-          },
-        ],
+      // {
+      //   name: "ROCKET LEAGUE",
+      //   disc: "",
+      //   BGImageLink: img15,
+      //   rule: [
+      //     {
+      //       rule_1: "MAX TEAM SIZE: 3",
+      //       rule_2: "The arena for each match WILL be predetermined",
+      //       rule_3: "TEAMS HAVE TO BRING THEIR OWN SETUP",
+      //       rule_4:
+      //         "In the event of a tied game , overtime periods are typically played until a golden goal (sudden-death goal) is scored",
+      //     },
+      //   ],
 
-        // BGImageLink: "",
-        PsLink: "",
-        coordinators: [
-          {
-            name: "Divyansh Gangwar",
-            phone: "9758987445",
-          },
-          {
-            name: "Nikhil Chaurasia",
-            phone: "9335183583",
-          },
-          {
-            name: "Shaurya Raj Srivastav",
-            phone: "8299760453",
-          },
-          {
-            name: "Utkarsh Chauhan",
-            phone: "9119391227",
-          },
-        ],
-      },
+      //   // BGImageLink: "",
+      //   PsLink: "",
+      //   coordinators: [
+      //     {
+      //       name: "Divyansh Gangwar",
+      //       phone: "9758987445",
+      //     },
+      //     {
+      //       name: "Nikhil Chaurasia",
+      //       phone: "9335183583",
+      //     },
+      //     {
+      //       name: "Shaurya Raj Srivastav",
+      //       phone: "8299760453",
+      //     },
+      //     {
+      //       name: "Utkarsh Chauhan",
+      //       phone: "9119391227",
+      //     },
+      //   ],
+      // },
       {
         name: "TEKKEN 8",
         disc: "",
@@ -418,6 +470,48 @@ export const Kreedomania = [
             rule_4: " Teams must not fix the outcome of any game(s)",
             rule5:
               " Teams must not use any unfair means. The team will be disqualified if the organisers find any team player using unfair means.",
+          },
+        ],
+
+        // BGImageLink: "",
+        PsLink: "",
+        coordinators: [
+          {
+            name: "Divyansh Gangwar",
+            phone: "9758987445",
+          },
+          {
+            name: "Nikhil Chaurasia",
+            phone: "9335183583",
+          },
+          {
+            name: "Shaurya Raj Srivastav",
+            phone: "8299760453",
+          },
+          {
+            name: "Utkarsh Chauhan",
+            phone: "9119391227",
+          },
+        ],
+      },
+      {
+        name: "FREE FIRE",
+        disc: "",
+        BGImageLink: img4,
+        rule: [
+          {
+            rule_1: "ALL THE TEAMS WILL BE DIVIDED INTO SUBGROUPS",
+            rule_2:
+              "3 BATTLE ROYALE WILL BE PLAYED AND YOU’LL BE AWARDED POINTS IN EACH BR ACCORDING TO THE FOLLOWING POINT SYSTEM - #1 - 15 POINTS #5 - 6 POINTS #2 - 12 POINTS #6 - 4 POINTS #3 - 10 POINTS #7 - 2 POINTS #4 - 8 POINTS #(8-12) - 1 POINTS (AND 1 POINT FOR EACH KILL FOR ALL TEAMS)",
+            rule_3:
+              "TOP 8 TEAMS WILL MOVE ON TO THE NEXT ROUND FROM EACH GROUP.",
+            rule_4:
+              "THE NEXT FINAL ROUND WILL BE A SERIES OF BATTLE ROYALE CONSISTING OF 3 ROUNDS.",
+            rule_5: "SAME POINT SYSTEM WILL BE FOLLOWED",
+            rule_6: "ORGANIZERS DECISIONS WILL BE FINAL",
+            rule_7: "SKIN ATTRIBUTES IS NOT ALLOWED",
+            rule_8:
+              "ROOM ID AND PASSWORD WILL BE SHARED 15 MINUTES PRIOR TO THE ALLOTED TIMINGS AND MATCHES WILL START ON TIME, WE’LL NOT WAIT FOR ANYONE.",
           },
         ],
 
